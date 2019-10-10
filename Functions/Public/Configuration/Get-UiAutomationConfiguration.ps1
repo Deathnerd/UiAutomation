e4ccff1207346ebe7d00025d66c5c9c5057273a2
Wes Gilleland <wgillela@opentext.com>
2019-10-10 12:07:29 -0400

    Initial commit

---
+++ Functions/Public/Configuration/Get-UiAutomationConfiguration.ps1
@@ -0,0 +1,12 @@
+﻿#requires -Modules Configuration
+function Get-UiAutomationConfiguration {
+    [CmdletBinding()]
+    Param()
+    Begin {
+    }
+    Process {
+        return Import-Configuration
+    }
+    End {
+    }
+}
---
+++ Functions/Public/Configuration/Set-UiAutomationConfiguration.ps1
@@ -0,0 +1,10 @@
+﻿#requires -Modules Configuration
+function Set-UiAutomationConfiguration {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+        [ValidateNotNullOrEmpty()]
+        [object]$InputObject
+    )
+    Export-Configuration -InputObject $InputObject -AsHashTable
+}
